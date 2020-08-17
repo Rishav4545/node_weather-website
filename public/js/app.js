4a54033a9265f6297723622b93b3138725be1b98
--global <rishav.basak45@gmail.com>
2020-08-17 21:45:42 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client server js is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg-1')
