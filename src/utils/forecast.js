ddaa1ebb5a1539cabcb5b29f7faad25b04b59e60
--global <rishav.basak45@gmail.com>
2020-08-18 10:51:49 +0530

    Adding humidity to a place

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude,longitude, callback) => {
             callback('Unable to find location!', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. But it feels like ' + body.current.feelslike)
+            callback(undefined, 'The humidity is ' + body.current.humidity + '%. And its ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. But it feels like ' + body.current.feelslike)
         }
     })
 }
